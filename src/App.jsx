2bb49e8406062561ac39ae22057506902f1a0921
Vince Vizza <vinny@vizza.dev>
2024-05-21 16:55:48 -0500

    add my tsx code to App.jsx and data to data.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,146 @@
+import { Helmet } from "https://esm.sh/react-helmet"
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { quotes, authors, colors } from './data.js'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [index, setIndex] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id="container">
+      <div id="quote-box" style={{ color: colors[index] }}>
+        <Quote quote={quotes[index]} />
+        <Author author={authors[index]} />
+        <Buttons
+          color={colors[index]}
+          quote={quotes[index]}
+          author={authors[index]}
+          onClick={() => {
+            setIndex(index === quotes.length - 1 ? 0 : index + 1);
+          }}
+        />
+        <Helmet>
+          {<style>{"body { background-color:" + colors[index] + "; }"}</style>}
+        </Helmet>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div id="footer">by vinnyv</div>
+    </div>
+  );
 }
 
-export default App
+interface Props {
+  quote: string;
+}
+
+function Quote({ quote }: Props) {
+  return (
+    <div id="text">
+      <p>{quote}</p>
+    </div>
+  );
+}
+
+interface Props {
+  author: string;
+}
+
+function Author({ author }: Props) {
+  return (
+    <div id="author">
+      <p>{"- " + author}</p>
+    </div>
+  );
+}
+
+/*interface Props {
+  color: string;
+  onClick: () => void;
+}*/
+
+function Buttons({ color, onClick, quote, author }: Props) {
+  return (
+    <div className="buttons">
+      <div
+        className="btn"
+        onClick={onClick}
+        id="new-quote"
+        title="See a new quote!"
+        style={{ backgroundColor: color }}
+      >
+        New Quote
+      </div>
+      <a
+        className="btn"
+        id="tweet-quote"
+        title="Tweet this quote!"
+        target="_blank"
+        href={'"https://twitter.com/intent/tweet?=' + quote + " -" + author}
+        style={{ backgroundColor: color }}
+      >
+        <i className="fa fa-twitter fa-lg"></i>
+      </a>
+      <a
+        className="btn"
+        id="insta-quote"
+        title="Post this quote on FB!"
+        target="_blank"
+        href="https://facebook.com"
+        style={{ backgroundColor: color }}
+      >
+        <i className="fa fa-facebook fa-lg"></i>
+      </a>
+    </div>
+  );
+}
+
+//export default App;
+
+//ReactDOM.render(<App />, document.getElementById("app"));
+
+/*ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);*/
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
---
+++ src/data.js
@@ -0,0 +1,35 @@
+let quotes = [
+    '"Destiny lies in the hands of men."',
+    '"Some people feel the rain, others just get wet."',
+    '"The only way to make sense out of change is to plunge into it, move with it, and join the dance."',
+    '"Happiness is when what you think, what you say, and what you do are in harmony."',
+    '"Life is like riding a bicycle. To keep your balance, you must keep moving."',
+    '"Look deep into nature, and then you will understand everything better."',
+    '"But the attitude of faith is to let go, and become open to truth, whatever it might turn out to be."',
+    '"To live is like to love - all reason is against it, and all healthy instinct for it."',
+    '"Wilderness is not a luxury but a necessity of the human spirit."'
+  ];
+
+  let authors = [
+    "Tagomi Nobuske",
+    "Bob Marley",
+    "Alan Watts",
+    "Mahatma Gandhi",
+    "Albert Einstein",
+    "Albert Einstein",
+    "Alan Watts",
+    "Samuel Butler",
+    "Edward Abbey"
+  ];
+  
+  const colors = [
+    "#fa7b7b",
+    "#fa9931",
+    "rgb(63, 61, 46)",
+    "#3ece28",
+    "#22d7d4",
+    "#0f63d9",
+    "#a771ff",
+    "#ff71a0",
+    "#0a6515"
+  ];
